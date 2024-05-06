9eea588291f45b4f587825c3e09dbdbc8743d510
bijan <7bijan@gmail.com>
2024-05-06 10:23:55 +0330

    Add bun

--- codegenScript.ts
+++ codegenScript.ts
@@ -9,7 +9,7 @@ dotenv.config({ path: envPath.resolve(__dirname, ".env") });
 const endpoint = process.env.NEXT_PUBLIC_DUBCRAFT_ENDPOINT || "https://dev-dubcraft.speax.ai/";
 
 // Prepare the command with new options from the updated package
-const openapiCommand = `npx --package @7nohe/openapi-react-query-codegen openapi-rq -i ${endpoint}apidocs/openapi.yaml -o src/services --request request.ts --format biome --lint biome --operationId --enums typescript`;
+const openapiCommand = `bun run openapi-rq -i ${endpoint}apidocs/openapi.yaml -o src/services --request request.ts --format biome --lint biome --operationId --enums typescript`;
 
 try {
   // Execute the command and inherit the standard output and error streams
