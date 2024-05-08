a541e66d48358ec1ecec27ec6067342cf938aa2a
bijan <7bijan@gmail.com>
2024-05-08 18:52:02 +0330

    add icon generator

---
+++ icongen.ts
@@ -0,0 +1,46 @@
+const fs =require("fs")
+const { promisify } =require("util")
+
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+
+type Icon = {
+  properties: {
+    name: string;
+  };
+};
+
+const camelize = (str: string): string => {
+  return str
+    .replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) => {
+      return index === 0 ? word.toLowerCase() : word.toUpperCase();
+    })
+    .replace(/\s+/g, "");
+};
+
+const generateIconTypes = async (file: string): Promise<void> => {
+  try {
+    const filePath = `src/components/icons/${file}.json`;
+    const data = await readFile(filePath, { encoding: "utf8" });
+    const icons: { icons: Icon[] } = JSON.parse(data);
+    const names = icons.icons.map((icon, idx) => `${idx !== 0 ? "\n\t" : ""}"${icon.properties.name}"`);
+
+    const content = `
+const ${camelize(file)}Names = [
+    ${names.join(",")}
+] as const;
+
+export type ${camelize(file)}Type = typeof ${camelize(file)}Names[number];
+`;
+
+    await writeFile(`src/components/icons/${camelize(file)}Names.ts`, content);
+  } catch (error) {
+    console.error(`Error generating icon types: ${error}`);
+  }
+};
+
+const files = ["Icons"];
+
+files.forEach((file) => {
+  generateIconTypes(file);
+});
---
+++ src/components/icons/index.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useTheme } from "@mui/material/styles";
+import IcomoonReact from "icomoon-react";
+import type { FC } from "react";
+
+import type { iconsType } from "./iconsNames";
+import iconSet from "./Icons.json";
+
+export interface IconProps {
+  color?: string;
+  size?: string | number;
+  name: iconsType;
+  className?: string;
+}
+
+export const Icon: FC<IconProps> = ({ name, size = 24, className, color }) => {
+  const theme = useTheme();
+
+  return (
+    <IcomoonReact
+      className={className}
+      iconSet={iconSet}
+      color={color ? (theme.palette as any)?.[`${color.split(".")[0]}`][`${color.split(".")[1]}`] : null}
+      size={size}
+      icon={name as string}
+    />
+  );
+};
