f562bafbe490f3c1afea0dcae1f216764261f80e
bij7n <7bijan@gmail.com>
2024-06-21 15:52:58 +0330

    update codegen for cms

--- codegenScript.ts
+++ codegenScript.ts
@@ -2,19 +2,29 @@ const { execSync } = require("child_process");
 const dotenv = require("dotenv");
 const envPath = require("path");
 
-// Load environment variables
 dotenv.config({ path: envPath.resolve(__dirname, ".env") });
 
-// Define the endpoint from environment variables or use a default
-const endpoint = process.env.NEXT_PUBLIC_MINECRAFT_ENDPOINT || "https://staging-minecraft.innercircle-chainmind.com/";
+const endpoints = [
+  {
+    name: "minecraft",
+    url: `${
+      process.env.NEXT_PUBLIC_MINECRAFT_ENDPOINT || "https://staging-minecraft.innercircle-chainmind.com/"
+    }apidocs/openapi.yaml`,
+  },
+  {
+    name: "cms",
+    url: `${process.env.NEXT_PUBLIC_CMS_ENDPOINT || "https://cms.innercircle-chainmind.com/"}"custom-api-docs/specs"`,
+  },
+];
 
-// Prepare the command with new options from the updated package
-const openapiCommand = `bun run openapi-rq -i ${endpoint}apidocs/openapi.yaml -o src/services --request request.ts --format biome --lint biome --operationId --enums typescript`;
-
-try {
-  // Execute the command and inherit the standard output and error streams
-  execSync(openapiCommand, { stdio: "inherit" });
-} catch (error) {
-  console.error("Error executing code generation script:", error);
-  process.exit(1);
-}
+endpoints.forEach((endpoint) => {
+  const openapiCommand = `bun run openapi-rq -i  ${endpoint.url} -o src/services/${endpoint.name} --request request.ts --format biome --lint biome --operationId --enums typescript`;
+  try {
+    execSync(openapiCommand, { stdio: "inherit" });
+    // biome-ignore lint/suspicious/noConsoleLog: <explanation>
+    console.log(`Successfully generated API client for ${endpoint.name}`);
+  } catch (error) {
+    console.error(`Error executing code generation script for ${endpoint.name}:`, error);
+    process.exit(1);
+  }
+});
