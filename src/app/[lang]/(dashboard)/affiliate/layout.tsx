29250f678a86f69f796f2dacb730b3518a424364
bijan <7bijan@gmail.com>
2024-06-23 07:47:54 +0330

    affiliate 404

--- src/app/[lang]/(dashboard)/affiliate/layout.tsx
+++ src/app/[lang]/(dashboard)/affiliate/layout.tsx
@@ -2,15 +2,17 @@ import type { PropsWithChildren } from "react";
 import { getQueryClient } from "@app/_providers/customQueryClient";
 import { prefetchUseAccountServiceAuthUserinfoQuery } from "@minecraft/queries/prefetch";
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import { useAccountServiceAuthUserinfoQueryKey } from "@minecraft/queries";
+import { notFound } from "next/navigation";
 // ----------------------------------------------------------------------
 
 export default async function AffiliateLayout({ children }: PropsWithChildren) {
   const queryClient = getQueryClient();
   await Promise.all([prefetchUseAccountServiceAuthUserinfoQuery(queryClient)]);
 
-  // if (!(queryClient.getQueryData([useAccountServiceAuthUserinfoQueryKey]) as any)?.data?.kyc_status) {
-  //   return notFound();
-  // }
+  if (!(queryClient.getQueryData([useAccountServiceAuthUserinfoQueryKey]) as any)?.data?.kyc_status) {
+    return notFound();
+  }
 
   return <HydrationBoundary state={dehydrate(queryClient)}>{children}</HydrationBoundary>;
 }
