511bb20b62249fafd16f47e640da44e44b82d34c
bij7n <7bijan@gmail.com>
2025-03-09 16:27:45 +0330

    fix(style): add global styles

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,10 @@
+import TwoFASubmitter from '@app-components/TwoFASubmitter';
+import DesktopSidebar from '@app-components/sidebar/Desktop';
 import { Stack } from '@mui/material';
 import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
-import TwoFASubmitter from '@app-components/TwoFASubmitter';
-import DesktopSidebar from '@app-components/sidebar/Desktop';
-import DashboardHeader from './dashboard/_section/Header';
 import PaymentNotice from './_section/PaymentNotice';
-import ScrollbarLayoutWraper from './_section/ScrollbarLayoutWraper';
+import DashboardHeader from './dashboard/_section/Header';
 // ----------------------------------------------------------------------
 
 export const metadata: Metadata = {
@@ -27,15 +26,13 @@ export default function DashboardLayout({ children, modal }: LayoutProps) {
 
         <Stack sx={{ flex: 1 }}>
           <DashboardHeader />
-          <ScrollbarLayoutWraper>
-            {/* Main content */}
-            <Stack>
-              <>
-                <PaymentNotice />
-                {children}
-              </>
-            </Stack>
-          </ScrollbarLayoutWraper>
+          {/* Main content */}
+          <Stack>
+            <>
+              <PaymentNotice />
+              {children}
+            </>
+          </Stack>
         </Stack>
       </Stack>
     </>
